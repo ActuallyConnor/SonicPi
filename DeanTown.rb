73f91302c2728f6554435a24b03293ad36f361bf
ActuallyConnor <connorsmyth@rogers.com>
2018-10-14 22:30:00 -0400

    adding first few bars of dean town

---
+++ DeanTown.rb
@@ -0,0 +1,109 @@
+use_synth :piano
+6.times do
+  4.times do
+    4.times do
+      play :Gb1
+      sleep 0.13
+    end
+  end
+end
+
+2.times do
+  4.times do
+    4.times do
+      play :Gb1
+      sleep 0.13
+    end
+  end
+end
+2.times do
+  4.times do
+    4.times do
+      play :Gb2
+      sleep 0.13
+    end
+  end
+end
+2.times do
+  4.times do
+    4.times do
+      play :Db2
+      sleep 0.13
+    end
+  end
+end
+2.times do
+  4.times do
+    4.times do
+      play :E2
+      sleep 0.13
+    end
+  end
+end
+2.times do
+  4.times do
+    4.times do
+      play :B2
+      sleep 0.13
+    end
+  end
+end
+
+2.times do
+  4.times do
+    4.times do
+      play :Gb1
+      sleep 0.13
+    end
+  end
+end
+2.times do
+  4.times do
+    4.times do
+      play :Gb2
+      sleep 0.13
+    end
+  end
+end
+2.times do
+  4.times do
+    4.times do
+      play :Db2
+      sleep 0.13
+    end
+  end
+end
+2.times do
+  4.times do
+    4.times do
+      play :E2
+      sleep 0.13
+    end
+  end
+end
+1.times do
+  4.times do
+    4.times do
+      play :B2
+      sleep 0.13
+    end
+  end
+end
+14.times do
+  play:B2
+  sleep 0.13
+end
+
+play :Eb2
+sleep 0.13
+play :Db2
+sleep 0.13
+play :E2
+sleep 0.13
+play :Eb2
+sleep 0.13
+play :Db2
+sleep 0.13
+play :A2
+sleep 0.13
+play :Gb1
